7c6907a9f3f5b0c67516b4c083cd2f1be4e2ba78
ohadApiiro <ohad@apiiro.com>
2023-11-15 11:26:55 +0200

    change version in nuget script

--- make-nuget.sh
+++ make-nuget.sh
@@ -7,26 +7,26 @@ WORK_DIR=`pwd`
 
 pushd ./src/LanguageServer/Impl
 dotnet pack --configuration Release
-dotnet nuget push $WORK_DIR/output/bin/Release/Microsoft.Python.LanguageServer.1.0.2.nupkg --source "github" --skip-duplicate --no-symbols true
+dotnet nuget push $WORK_DIR/output/bin/Release/Microsoft.Python.LanguageServer.1.0.3.nupkg --source "github" --skip-duplicate --no-symbols true
 popd
 
 pushd ./src/Analysis/Ast/Impl
 dotnet pack --configuration Release
-dotnet nuget push $WORK_DIR/output/bin/Release/Microsoft.Python.Analysis.1.0.2.nupkg --source "github" --skip-duplicate --no-symbols true
+dotnet nuget push $WORK_DIR/output/bin/Release/Microsoft.Python.Analysis.1.0.3.nupkg --source "github" --skip-duplicate --no-symbols true
 popd
 
 pushd ./src/Analysis/Core/Impl
 dotnet pack --configuration Release
-dotnet nuget push $WORK_DIR/output/bin/Release/Microsoft.Python.Analysis.Core.1.0.2.nupkg --source "github" --skip-duplicate --no-symbols true
+dotnet nuget push $WORK_DIR/output/bin/Release/Microsoft.Python.Analysis.Core.1.0.3.nupkg --source "github" --skip-duplicate --no-symbols true
 popd
 
 pushd ./src/Parsing/Impl
 dotnet pack --configuration Release
-dotnet nuget push $WORK_DIR/output/bin/Release/Microsoft.Python.Parsing.1.0.2.nupkg --source "github" --skip-duplicate --no-symbols true
+dotnet nuget push $WORK_DIR/output/bin/Release/Microsoft.Python.Parsing.1.0.3.nupkg --source "github" --skip-duplicate --no-symbols true
 popd
 
 pushd ./src/Core/Impl
 dotnet pack --configuration Release
-dotnet nuget push $WORK_DIR/output/bin/Release/Microsoft.Python.Core.1.0.2.nupkg --source "github" --skip-duplicate --no-symbols true
+dotnet nuget push $WORK_DIR/output/bin/Release/Microsoft.Python.Core.1.0.3.nupkg --source "github" --skip-duplicate --no-symbols true
 popd
 
